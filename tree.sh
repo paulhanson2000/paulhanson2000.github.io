06611c993ac81139597db61fe43c7b1e3febc05d
paulhanson2000 <paulhanson2000@gmail.com>
2024-02-16 12:23:20 -0500

    Add metal_script.txt

--- tree.sh
+++ tree.sh
@@ -5,6 +5,6 @@ tree -H '.' \
     -I "index.html|*_files" \
     -T 'Data download for the CDSI Genomic Meta-Analysis workshop' \
     -s -D \
-    -P "*.zip" \
+    -P "data.zip|metal_script.txt" \
     -o index.html
 
